4474326342217e628ecb8dc7bda39bdbcc38c314
kwun lok yuen <vyuen1000@gmail.com>
2024-01-31 20:17:34 -0600

    first commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// import React from 'react';
+import Header from "./Header";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+const App: React.FC = () => {
+  return (
+    <div>
+      <Header />
+
+      {/* Other content of your application */}
+    </div>
+  );
+};
+export default App;
---
+++ my-app/src/Header.tsx
@@ -0,0 +1,31 @@
+// Header.tsx
+import React from "react";
+import Stickynode from "react-stickynode";
+import styled from "styled-components";
+
+// Define styles for your header using styled-components
+const HeaderWrapper = styled.div`
+  background-color: #333;
+  color: white;
+  padding: 10px 20px;
+  position: relative;
+  top: 0;
+
+  z-index: 999; /* Ensures header is above other content */
+`;
+
+const HeaderTitle = styled.h1`
+  margin-left: 200px; /* Adjust the spacing as per your requirement */
+`;
+
+const Header: React.FC = () => {
+  return (
+    <Stickynode top={0} innerZ={999}>
+      <HeaderWrapper>
+        <HeaderTitle>Your Header Content</HeaderTitle>
+      </HeaderWrapper>
+    </Stickynode>
+  );
+};
+
+export default Header;
