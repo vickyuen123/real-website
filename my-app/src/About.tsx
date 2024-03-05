bbe3834afda5a495b6eebd8c8bee98aeca70c7ff
victor <vyuen1000@gmail.com>
2024-03-04 21:01:58 -0600

    about page

--- my-app/src/About.tsx
+++ my-app/src/About.tsx
@@ -4,43 +4,50 @@ import styled from "styled-components";
 import Header from "./Header";
 import Footer from "./Footer";
 
-const Test = styled.div`
+const AboutWrapper = styled.div`
   margin-left: 200px; /* Sets margin on all sides */
   margin-top: 100px;
   margin-right: 200px;
   margin-bottom: 100px;
-  padding: 20px; /* Adding some padding to see the content */
+  gap: 20px;
+  padding: 10px 20px;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  /* Adding some padding to see the content */
 `;
 
-const Marg = styled.div`
-  margin-left: 20px; /* Sets margin on all sides */
+function getLoremIpsumText() {
+  return `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+  occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+  mollit anim id est laborum.`;
+}
 
-  margin-right: 20px;
-
-  padding: 20px;
-`;
-
-const Photo = styled.img`
-  src="https://gifdb.com/images/high/spongebob-kiss-my-sponge-jj8sfknlmk53cazv.gif",
-
-  
-  style={{ width: "100%", height: "auto" }}
-
-`;
 const About: React.FC = () => {
   return (
     <div>
       <Header />
-      <Test>
-        <Marg>
-          <img
-            src="https://headshots-inc.com/wp-content/uploads/2023/02/Professional-Headshot-Photography-Example-1.jpg"
-            alt="Category 11"
-            style={{ width: "25%", height: "auto" }}
-          />
-        </Marg>
-        hello
-      </Test>
+      <AboutWrapper>
+        <img
+          src="https://headshots-inc.com/wp-content/uploads/2023/02/Professional-Headshot-Photography-Example-1.jpg"
+          alt="Category 11"
+          style={{ width: "200px", height: "200px" }}
+        />
+        <p>{getLoremIpsumText()}</p>
+      </AboutWrapper>
+
+      <AboutWrapper>
+        <p>{getLoremIpsumText()}</p>
+        <img
+          src="https://headshots-inc.com/wp-content/uploads/2023/02/Professional-Headshot-Photography-Example-1.jpg"
+          alt="Category 11"
+          style={{ width: "200px", height: "200px" }}
+        />
+      </AboutWrapper>
 
       <Footer />
       {/* Other content of your application */}
